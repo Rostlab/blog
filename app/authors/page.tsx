14b9ba4c81222cfdfd5be0d52a71d10b35560e01
Tobias O <tobias.olenyi@tum.de>
2023-07-20 22:16:16 +0200

    Add multiple author layout

--- app/authors/page.tsx
+++ app/authors/page.tsx
@@ -11,10 +11,27 @@ export default function Page() {
   const mainContent = coreContent(author)
 
   return (
-    <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
-    </>
+    <section className="body-font text-gray-600">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Our Team
+        </h1>
+      </div>
+      {!allAuthors.length && 'No authors found.'}
+      {allAuthors.length > 0 && (
+        <div className="container mx-auto px-5 py-24">
+          <div className="-m-4 flex flex-wrap">
+            {allAuthors.map((author) => {
+              const mainContent = coreContent(author)
+              return (
+                <AuthorLayout key={author._id} content={mainContent}>
+                  <MDXLayoutRenderer code={author.body.code} />
+                </AuthorLayout>
+              )
+            })}
+          </div>
+        </div>
+      )}
+    </section>
   )
 }
