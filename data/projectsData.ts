62059fa4a38ba2f920919de25c6b29e6345efee2
Tobias O <tobias.olenyi@tum.de>
2023-07-23 23:02:56 +0200

    Add winter school reference

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,3 +1,10 @@
-const projectsData = []
+const projectsData = [
+  {
+    title: 'Winter School for Toxinologists 2023',
+    description: `You have a bio background and want to learn Machine Learning. We got you covered.`,
+    imgSrc: '/static/images/tum-staircase.jpg',
+    href: 'https://mlschool23.rostlab.org',
+  },
+]
 
 export default projectsData
