c05048a5310c797d9e1e1ebb4a687bda0c624cb1
Tobias O <tobias.olenyi@tum.de>
2023-07-25 16:54:23 +0200

    Change Metadata to authors

--- app/authors/page.tsx
+++ app/authors/page.tsx
@@ -4,7 +4,7 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({ title: 'Authors' })
 
 export default function Page() {
   return (
